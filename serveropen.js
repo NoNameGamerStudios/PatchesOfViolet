ce0c920c93fa6e277a8efcca0b277465522c504b
No Name Gamer Studios <nonamegameronline@gmail.com>
2025-11-06 22:18:47 -0500

    Attempted to simplify

--- serveropen.js
+++ serveropen.js
@@ -1,17 +1,7 @@
 addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("makeServer").onclick = readInformation;
+    document.getElementById("makeServer").onclick = declareEnv;
 
-    function readInformation() {
-        const ClientDate = new Date();
-        let ClientUsername = document.getElementById("UsernameInput").value;
-        let ClientSession = ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
-        console.log("User Session ID is " + ClientSession);
-
-        declareEnv();
-        createServerVisiblity();
-        
-        return ClientSession;
-    }
+  
     function declareEnv() {
         const date = new Date();
         let ClientUsername = document.getElementById("UsernameInput").value;
@@ -30,9 +20,7 @@ addEventListener("DOMContentLoaded", (event) => {
             console.info("Client Username Is " + ClientUsername + " Client Session Is " + ClientSession);
             console.info("Server Title Is " + serverTitle);
         }
-    }
-    function createServerVisiblity() {
-        console.error(ClientSession);
+
         peer.on('connection', function(conn) { 
             console.log("A connection has been made.");
          });
