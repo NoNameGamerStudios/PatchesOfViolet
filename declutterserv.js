396c4c97e585763c3a22ce3df7e6667324d54464
No Name Gamer Studios <nonamegameronline@gmail.com>
2025-11-06 21:44:31 -0500

    Test UsernameSession Fix (
    Test UsernameSession Fix ( i know fomatting sucks i'll fix it next update)

--- declutterserv.js
+++ declutterserv.js
@@ -1,12 +1,14 @@
 addEventListener("DOMContentLoaded", (event) => {
-    const ClientDate = new Date();
+   
+
+    document.getElementById("scanNetwork").onclick = initContext;
+    
+
+    function initContext() {
+         const ClientDate = new Date();
     let ClientUsername = document.getElementById("UsernameInput").value;
     let ClientSession = ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
 
-    document.getElementById("scanNetwork").onclick = initContext;
-    let peer = new Peer(ClientSession);
-
-    function initContext() {
         let peer = new Peer(ClientSession);
         console.log(ClientSession + " Is the standard client session ID");
         peer.on("open", function (id) {
