e1bc797c8c88219cffe857a51a4c0beadd95b276
No Name Gamer Studios <nonamegameronline@gmail.com>
2025-11-06 22:00:47 -0500

    Attempt basic serverside info
    Yes Cool Awesome
    - The One In Charge

--- declutterserv.js
+++ declutterserv.js
@@ -6,13 +6,16 @@ addEventListener("DOMContentLoaded", (event) => {
 
     function initContext() {
          const ClientDate = new Date();
-    let ClientUsername = document.getElementById("UsernameInput").value;
-    let ClientSession = ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
+        let ClientUsername = document.getElementById("UsernameInput").value;
+        let ClientSession = "Patch" + ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
 
         let peer = new Peer(ClientSession);
         console.log(ClientSession + " Is the standard client session ID");
         peer.on("open", function (id) {
             console.log("My peer ID is: " + id);
+            // Below code should allow for connections to a specific ID
+            var conn = peer.connect('dest-peer-id');
+
         });
     }
 });
