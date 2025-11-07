1428ed3809b9889d4eccf1023a9337503dfb2f9a
No Name Gamer Studios <nonamegameronline@gmail.com>
2025-11-06 22:57:34 -0500

    Cleaning up code
    Meant to clean code

--- declutterserv.js
+++ declutterserv.js
@@ -1,30 +1,70 @@
-addEventListener("DOMContentLoaded", (event) => {
-   
+document.getElementById("scanNetwork").onclick = initContext;
+document.getElementById("makeServer").onclick = serverSide;
 
-    document.getElementById("scanNetwork").onclick = initContext;
-    
 
-    function initContext() {
-         const ClientDate = new Date();
-        let ClientUsername = document.getElementById("UsernameInput").value;
-        let ClientSession = "Patch" + ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
-        let connectionAddress = document.getElementById("destinationAddress").value;
+function initContext() {
+    const ClientDate = new Date();
+    let ClientUsername = document.getElementById("UsernameInput").value;
+    let ClientSession = "Patch" + ClientUsername + ClientDate.getDate() + ClientDate.getMilliseconds();
+    let connectionAddress = document.getElementById("destinationAddress").value;
 
-        let peer = new Peer(ClientSession);
-        console.log(ClientSession + " Is the standard client session ID");
-        peer.on("open", function (id) {
-            console.log("My peer ID is: " + id);
-            // Below code should allow for connections to a specific ID
-            var conn = peer.connect(connectionAddress);
-            conn.on('open', function() {
-	      // Receive messages
-	     conn.on('data', function(data) {
-	       console.log('Received', data);
-	     });
+    let peer = new Peer(ClientSession);
+    console.log(ClientSession + " Is the standard client session ID");
+    peer.on("open", function(id) {
+        console.log("My peer ID is: " + id);
+        // Below code should allow for connections to a specific ID
+        var conn = peer.connect(connectionAddress);
+        conn.on('open', function() {
+            // Receive messages
+            conn.on('data', function(data) {
+                console.log('Received', data);
+            });
 
-	     // Send messages
-    	    conn.send('Hi! My User Is ' + ClientUsername);
-    });
+            // Send messages
+            conn.send('Hi! My User Is ' + ClientUsername);
         });
+    });
+}
+
+
+
+
+// Server Logic
+
+function serverSide() {
+    const date = new Date();
+    let ClientUsername = document.getElementById("UsernameInput").value;
+    let ClientSession = ClientUsername + date.getDate() + date.getMilliseconds();
+    // Server And Client Information
+    const serverID = "Server" + ClientSession;
+
+    const serverStartDate = date.getHours() + ":" + date.getMinutes();
+
+    const serverTitle = ClientUsername + "'s Server";
+    if (!ClientUsername.trim()) {
+        alert("Please Create A Username");
+    } else {
+        console.info("Server ID is:" + serverID);
+        console.info("Server Start Date is " + serverStartDate);
+        console.info("Client Username Is " + ClientUsername + " Client Session Is " + ClientSession);
+        console.info("Server Title Is " + serverTitle);
     }
-});
+
+    let peer = new Peer(serverID);
+
+
+
+    conn.on('open', function() {
+        // Receive messages
+        conn.on('data', function(data) {
+            console.log('Received', data);
+        });
+
+        // Send messages
+        conn.send('Hi! My User Is ' + ClientUsername);
+    });
+
+    peer.on('connection', function(conn) {
+        console.log("A connection has been made.");
+    });
+}
